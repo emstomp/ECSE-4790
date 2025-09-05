131003d82174fbd61256ba0b561a062946911683
Andrii Bonenberger <tychea@rpi.edu>
2025-09-05 13:13:17 -0400

    IT WORKS!!!

--- hello.c
+++ hello.c
@@ -18,12 +18,38 @@
 #include "stm32f769xx.h"
 #include "hello.h"
 
+uint8_t fault =0 ;
 #include <stdint.h>
 
 #define ESC_KEY 27
 void drawScreen();
 char arr[10][72];
 uint16_t id=0;
+uint16_t PrintChar=0;
+uint16_t NotPrintChar=0;
+char badList[22]={127,9,12,13,16,17,18,19,20,33,34,35,36,37,38,39,40};
+//8 - Backspace (127)
+//9 - Tab
+//12 - 5 in the numeric keypad when Num Lock is off
+//13 - Enter
+//16 - Shift
+//17 - Ctrl
+//18 - Alt
+//19 - Pause/Break
+//20 - Caps Lock
+//27 - Esc
+//32 - Space
+//33 - Page Up
+//34 - Page Down
+//35 - End
+//36 - Home
+//37 - Left arrow
+//38 - Up arrow
+//39 - Right arrow
+//40 - Down arrow
+//44 - Print Screen
+//45 - Insert
+//46 - Delete
 
 char inputChar;
 //------------------------------------------------------------------------------------
@@ -42,45 +68,14 @@ int main(void)
                     // ANY printf() call, not just ones that contain a '\n'.
 
 
+    HAL_Delay(1000); // Pause for a second. This function blocks the program and uses the SysTick and
     // Need to enable clock for peripheral bus on GPIO Port J
     __HAL_RCC_GPIOJ_CLK_ENABLE(); 	// Through HAL
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOJEN; // or through registers
-    //// Below two lines are example on how to access a register by knowing it's memory address
-    //volatile uint32_t * RCC_AHB1_CLOCK_ENABLE = (uint32_t*) 0x40023830U; // Enable clock for peripheral bus on GPIO Port J
-    //*RCC_AHB1_CLOCK_ENABLE |= 512U; // Bitmask for RCC AHB1 initialization: 0x00000200U or 512U in decimal
-
-
-
-// It doesn't get lower level than this!
-
-
-    //// This is an example of how to create a pointer to access a GPIO register.
-    //// It is strongly recommended to follow the register access above (GPIOJ->...) and not this!!
-    //// Access GPIOJ MODER register
-    // volatile uint32_t * GREENLEDMODER = (uint32_t*) 0x40022400U; // Init GPIO J Pin 5 (LED2 with no Alt. func.) to Output
-    // *GREENLEDMODER |= 1024U; // Bitmask for GPIO J Pin 5 initialization: 0x00000400U or 1024U in decimal
-//    // Access GPIOJ BSRR register
-//     volatile uint32_t * GREENLEDBSRR = (uint32_t*) 0x40022418U; // Address of GPIO J Bit Set/Reset Register
-//     *GREENLEDBSRR = (uint16_t)0x0020U; // Turn on Green LED (LED2)
-
-    HAL_Delay(1000); // Pause for a second. This function blocks the program and uses the SysTick and
-                     // associated handler to keep track of time.
-
-//    volatile uint32_t * GREENLEDODR = (uint32_t*) 0x40022414U; // Address of GPIO J Output Data Register
-//    *GREENLEDODR ^= (uint16_t)0x0020U; // Toggle Green LED (LED2)
-
-//    printf("\0333[44;33m"); // blue background and yellow characters
-//    fflush(stdout);
-    // TODO: center instruction text on line 2. cols: 80, rows: 24
-
-	//initiate all the data as period
-
-	drawScreen();
 	GPIOJ->MODER |= 1024U; //Bitmask for GPIO J Pin 5 initialization (set it to Output mode): 0x00000400U or 1024U in decimal
 	GPIOJ->BSRR = (uint16_t)GPIO_PIN_5; // Turn on Green LED (LED2)
 	GPIOJ->BSRR = (uint32_t)GPIO_PIN_5 << 16; // Turn off Green LED (LED2)
 	GPIOJ->ODR ^= (uint16_t)GPIO_PIN_5; // Toggle LED2
-
 	for (int i = 0; i < 10; i++) {
 	    for (int j = 0; j < 72; j++) {
 	        arr[i][j] = '.';
@@ -89,90 +84,114 @@ int main(void)
 	}
     while(1)
     {
+
     	drawScreen();
-    	inputChar = getchar();
-    	HAL_Delay(100);
 
-    	if (id >= 720){
-    		id = id - 73;
-    		for(int i=0;i<9;i++){
-    			for(int j=0;j<72;j++){
-    				arr[i][j] = arr[i+1][j];//move all the data one row down.
-
-    			};
-    		};
-			for(int j=0;j<72;j++){
-				arr[9][j] = 46;//46 is asci for period
-			};
-
-    		//make it erase the first row and do the data thing
-    	}else{
-    		//save the character into the respective array
-    		arr[id/72][id%72]=inputChar;
+    	//StackOverflow solution to the buffer overflowing, according to the guy who posted this "When I wrote this me and god knew how it works, now its just god"
+    	// Clear Overrun Error if it happened
+    	if (USART1->ISR & USART_ISR_ORE) {
+    	    volatile char dump = (char)USART1->RDR;     // read RDR to clear
+    	    (void)dump;
+    	    USART1->ICR |= USART_ICR_ORECF;             // clear the overrun flag
     	}
 
-    	id++;
 
 
+    	inputChar = getchar();
 
+    	if (inputChar == 27){
+    		break;
+    	}
 
+    	uint8_t good = 1;
+    	for(int k=0;k<22;k++){
+    		if (inputChar == badList[k]){
+    			good = 0;//Bools would be useful here but eh
+    		}
+    	}
 
+    	if(good==1){
+    		fault = 0;
+    		PrintChar++;
+			if (id >= 720){
+				id = id - 73;
+				for(int i=0;i<9;i++){
+					for(int j=0;j<72;j++){
+						arr[i][j] = arr[i+1][j];//move all the data one row down.
 
+					};
+				};
+				for(int j=0;j<72;j++){
+					arr[9][j] = 46;//46 is asci for period
+				};
+			}else{
+				//save the character into the respective array
+				arr[id/72][id%72]=inputChar;
+			}
+			id++;
+    	}else{
+    		fault =1;
+    		NotPrintChar++;
+    		drawScreen();
 
-
-    	/** // Hello world
-    	 * printf("Hello World!\r\n\n");
-    	 * printf("( Welcome to Microprocessor Systems )\r\n\n\n");
-    	 * printf("1=repeat, 2=clear, 0=quit.\r\n\n"); // Menu of choices
-
-    	 * choice = getchar();
-    	 * putchar(choice);
-
-    	 * // select which option to run
-    	 * // *GREENLEDBSRR = (uint16_t)0x0020U; // Turn on Green LED (LED2)
-    	 * GPIOJ->BSRR = (uint16_t)0x0020U; // Turn on Green LED (LED2)
-    	 * // HAL_GPIO_WritePin(GPIOJ, GPIO_PIN_5, GPIO_PIN_SET);         // Turn green LED on (GPIO_PIN_SET == 1)
-    	 * if (choice == '0')
-         *   return 1;
-         * else if(choice == '1')
-         *   printf("\r\n\nHere we go again.\r\n\n");
-         * else if(choice == '2')          // clear the screen with <ESC>[2J
-         * {
-         *   printf("\033[2J\033[;H");
-         *   fflush(stdout);
-         * }
-         * else
-         * {
-         *   // Turn OFF LED to indicate incorrect selection
-         *   // *GREENLEDBSRR = (uint32_t)0x0020U << 16; // Turn off Green LED (LED2)
-         *	GPIOJ->BSRR = (uint16_t)0x0020U << 16; // Turn on Green LED (LED2)
-         *   printf("\r\nA \"");
-         *	putchar(choice);
-         *   printf("\" is not a valid choice.\r\n\n");
-         * }
-    	 **/
+    	}
 
     }
 }
 
-void drawScreen(){
-
+void drawScreen(){ //idk why the pointer fixes it but stackoverflow knows better then me
+	printf("\033[?25l"); // make the cursor invisible, just less jumpy
 	printf("\033[38;5;220m");//foreground
     printf("\033[48;5;24m");//background
     printf("\033[2;19H");
-    printf("Enter <ESC> or <CTRL> + [ to terminate, id=%d\r\n\n",id);
+    printf("Enter <ESC> or <CTRL> + [ to terminate\r\n\n");
+
     printf("\033[3;0H");
-    for(int i =0;i<80;i++){
+    for(int i =0;i<80;i++){// line of dashes to show the start of the printable zone
     	printf("-");
     }
+
+
     for (int i = 0; i < 10; i++) {
         printf("\033[%d;5H", i+4);          // move to row i+4, col 4
         printf("%.*s", 72, arr[i]);         // print 72 chars from arr[i] in one go
     }
+
+
     printf("\033[14;0H");
-    for(int i =0;i<80;i++){
+    for(int i =0;i<80;i++){	// line of dashes to show the end of the printable zone
         	printf("-");
         }
+
+
+    if (fault == 1){
+    	printf("\033[16;0H");
+    	printf("\033[38;5;196m");//foreground red
+    	printf("The received value $%x is ’not printable’",inputChar);
+    	printf("\033[38;5;220m");//foreground normal
+    }else{
+    	printf("\033[16;0H");
+		for(int i =0;i<80;i++){	// line of dashes to show the end of the printable zone
+				printf(" ");
+			}
+    }
+
+
+	printf("\033[21;0H");
+	printf("# of Characters Received:");
+	printf("\033[22;0H");
+	printf("Printable             Non-Printable");
+	printf("\033[23;0H");
+	printf("%d",PrintChar);
+	printf("\033[23;22H");
+	printf("%d",NotPrintChar);
+
+
+
+
+
+    printf("\033[?25h"); // make the cursor visible,
     fflush(stdout);
+
 //    HAL_Delay(100);
 }
